3ca001e582ef0380a7b4f7f0925f47361ef662d5
Sergey Antipov <72650361+AntipovSergey@users.noreply.github.com>
2021-08-06 13:14:10 +0300

    Выполняет верстку, стилизацию и адаптацию footer

--- source/js/script.js
+++ source/js/script.js
@@ -5,7 +5,6 @@ const menuBtn = document.querySelector('.main-header__burger');
 const menuClose = document.querySelector('.main-header__close');
 const menu = document.querySelector('.main-header__site-navigation');
 
-console.log(mainNav)
 mainNav.classList.remove('main-header__nav--nojs');
 
 menuBtn.addEventListener('click', () => {
