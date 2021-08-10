5557039b19f3281d02b10ff3de37a92fb930cdfd
Sergey Antipov <72650361+AntipovSergey@users.noreply.github.com>
2021-08-10 11:36:21 +0300

    Завершил JS (скролл меню со странами и карточки со странами)

--- source/js/script.js
+++ source/js/script.js
@@ -153,7 +153,126 @@ buttonForm.addEventListener('click', (evt) => {
     evt.preventDefault();
     overlay.classList.add('overlay--shown');
     showPopupSuccess();
+    form.reset();
   } else {
     form.reset();
   }
 })
+
+// переключение карточек стран
+const countryCards = document.querySelectorAll('.sites__link')
+const countryLinks = document.querySelectorAll('.country__link');
+const countryArticles = document.querySelectorAll('.country__article');
+
+countryArticles.forEach((article) => {
+  article.classList.add('country__article--hidden');
+});
+countryLinks[0].classList.add('country__item--selected');
+countryArticles[0].classList.remove('country__article--hidden');
+
+const changeArticles = (index) => {
+  countryLinks.forEach((button) => {
+    button.classList.remove('country__item--selected');
+  });
+  countryArticles.forEach((article) => {
+    article.classList.add('country__article--hidden');
+  });
+
+  countryLinks[index].classList.add('country__item--selected');
+  countryArticles[index].classList.remove('country__article--hidden');
+};
+
+countryLinks.forEach((element, index) => {
+  element.addEventListener('click', (evt) => {
+    evt.preventDefault();
+    changeArticles(index);
+  });
+});
+
+countryCards.forEach((element, index) => {
+  element.addEventListener('click', () => {
+  changeArticles(index);
+  });
+});
+
+//Скролл меню country__list (библиотека Dragscroll)
+
+(function (root, factory) {
+  if (typeof define === 'function' && define.amd) {
+      define(['exports'], factory);
+  } else if (typeof exports !== 'undefined') {
+      factory(exports);
+  } else {
+      factory((root.dragscroll = {}));
+  }
+}(this, function (exports) {
+  let _window = window;
+  let _document = document;
+  let mousemove = 'mousemove';
+  let mouseup = 'mouseup';
+  let mousedown = 'mousedown';
+  let EventListener = 'EventListener';
+  let addEventListener = 'add'+EventListener;
+  let removeEventListener = 'remove'+EventListener;
+  let newScrollX, newScrollY;
+
+  let dragged = [];
+  let reset = function(i, el) {
+      for (i = 0; i < dragged.length;) {
+          el = dragged[i++];
+          el = el.container || el;
+          el[removeEventListener](mousedown, el.md, 0);
+          _window[removeEventListener](mouseup, el.mu, 0);
+          _window[removeEventListener](mousemove, el.mm, 0);
+      }
+
+      // cloning into array since HTMLCollection is updated dynamically
+      dragged = [].slice.call(_document.getElementsByClassName('dragscroll'));
+      for (i = 0; i < dragged.length;) {
+          (function(el, lastClientX, lastClientY, pushed, scroller, cont){
+              (cont = el.container || el)[addEventListener](
+                  mousedown,
+                  cont.md = function(e) {
+                      if (!el.hasAttribute('nochilddrag') ||
+                          _document.elementFromPoint(
+                              e.pageX, e.pageY
+                          ) == cont
+                      ) {
+                          pushed = 1;
+                          lastClientX = e.clientX;
+                          lastClientY = e.clientY;
+
+                          e.preventDefault();
+                      }
+                  }, 0
+              );
+
+              _window[addEventListener](
+                  mouseup, cont.mu = function() {pushed = 0;}, 0
+              );
+
+              _window[addEventListener](
+                  mousemove,
+                  cont.mm = function(e) {
+                      if (pushed) {
+                          (scroller = el.scroller||el).scrollLeft -=
+                              newScrollX = (- lastClientX + (lastClientX=e.clientX));
+                          if (el == _document.body) {
+                              (scroller = _document.documentElement).scrollLeft -= newScrollX;
+                          }
+                      }
+                  }, 0
+              );
+          })(dragged[i++]);
+      }
+  }
+
+
+  if (_document.readyState == 'complete') {
+      reset();
+  } else {
+      _window[addEventListener]('load', reset, 0);
+  }
+
+  exports.reset = reset;
+}));
